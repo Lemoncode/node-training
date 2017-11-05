f76718f3c2d0f46b508b205237a79632206d84f4
nasdan <daniel.sanchez@lemoncode.net>
2017-11-05 23:23:52 +0100

    Implement nextTick sample

--- 02 Concurrency Model and Event Loop/02 NextTick/index.js
+++ 02 Concurrency Model and Event Loop/02 NextTick/index.js
@@ -1,10 +1,25 @@
 const fs = require('fs');
 
-fs.readFile(__filename, () => {
-  setTimeout(() => {
-    console.log('timeout');
-  }, 0);
-  setImmediate(() => {
-    console.log('immediate');
+function fileSize(fileName, cb) {
+  if (typeof fileName !== 'string') {
+    return process.nextTick(cb, new TypeError('args should be string'));
+  }
+
+  fs.stat(fileName, (err, stats) => {
+    if (err) {
+      return cb(err);
+    }
+
+    cb(null, stats.size);
   });
+}
+
+fileSize(1, (err, size) => {
+  if (err) {
+    throw err;
+  }
+
+  console.log(`Size in KB: ${size / 1024}`);
 });
+
+console.log('Hello!');
