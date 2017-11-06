0ed2a4d9da4ae8b927898c591e2b0d9d8278711e
nasdan <daniel.sanchez@lemoncode.net>
2017-11-06 22:37:29 +0100

    Implement sample and update readme

---
+++ 03 Events/03 Sync to async class/async.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const EventEmitter = require('events');
+
+class WithTime extends EventEmitter {
+  execute(asyncFunc, ...args) {
+    console.time('Executed in');
+    asyncFunc(...args, (err, data) => {
+      if (err) {
+        return this.emit('error', err);
+      }
+
+      this.emit('data', data);
+      console.timeEnd('Executed in');
+      this.emit('end');
+    });
+  }
+}
+
+const withTime = new WithTime();
+
+withTime.on('data', (data) => console.log(data));
+withTime.on('error', (error) => console.error(error));
+withTime.on('end', () => console.log('end'));
+
+withTime.execute(fs.readFile, __filename);
