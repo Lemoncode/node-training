d145a827aa75a173ada41b407bde0a6e7da7e4ec
nasdan <daniel.sanchez@lemoncode.net>
2017-11-08 18:27:07 +0100

    Implement sample

--- 04 Web/01 Http Server Stream/index.js
+++ 04 Web/01 Http Server Stream/index.js
@@ -2,7 +2,15 @@ const server = require('http').createServer();
 
 server.on('request', (req, res) => {
   res.writeHead(200, { 'Conent-Type': 'text/plain' });
-  res.end('Hello World\n');
+  res.write('Hello World\n');
+
+  setTimeout(() => {
+    res.write('Another Hello World\n');
+  }, 1000);
+
+  setTimeout(() => {
+    res.end('...Another Hello World\n');
+  }, 2000);
 });
 
 server.listen(3000);
