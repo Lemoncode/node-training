6a3851660b76f657f8d5296da8f25b4ae9c00ad0
nasdan <daniel.sanchez@lemoncode.net>
2017-11-14 14:15:21 +0100

    Implement sample

--- 06 Streams/04 Duplex/index.js
+++ 06 Streams/04 Duplex/index.js
@@ -1,10 +1,24 @@
-const { Writable } = require('stream');
+const { Duplex } = require('stream');
 
-const outStream = new Writable({
+const inOutStream = new Duplex({
   write(chunk, encoding, callback) {
     console.log(chunk.toString());
     callback();
+  },
+
+  read(size) {
+    setTimeout(() => {
+      if (this.currentCharCode > 90) {
+        this.push(null);
+        return;
+      }
+      this.push(String.fromCharCode(this.currentCharCode++));
+    }, 100);
   }
 });
 
-process.stdin.pipe(outStream);
+inOutStream.currentCharCode = 65;
+
+process.stdin
+  .pipe(inOutStream)
+  .pipe(process.stdout);
