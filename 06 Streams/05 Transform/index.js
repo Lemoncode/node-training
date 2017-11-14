b01de2032f92012838ce2bd822daad2700af5dc7
nasdan <daniel.sanchez@lemoncode.net>
2017-11-14 14:27:29 +0100

    Implement sample

--- 06 Streams/05 Transform/index.js
+++ 06 Streams/05 Transform/index.js
@@ -1,24 +1,12 @@
-const { Duplex } = require('stream');
+const { Transform } = require('stream');
 
-const inOutStream = new Duplex({
-  write(chunk, encoding, callback) {
-    console.log(chunk.toString());
+const upperCaseTransform = new Transform({
+  transform(chunck, encoding, callback) {
+    this.push(chunck.toString().toUpperCase());
     callback();
-  },
-
-  read(size) {
-    setTimeout(() => {
-      if (this.currentCharCode > 90) {
-        this.push(null);
-        return;
-      }
-      this.push(String.fromCharCode(this.currentCharCode++));
-    }, 100);
   }
 });
 
-inOutStream.currentCharCode = 65;
-
 process.stdin
-  .pipe(inOutStream)
+  .pipe(upperCaseTransform)
   .pipe(process.stdout);
